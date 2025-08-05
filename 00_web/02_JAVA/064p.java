29454840c7febbe2e1b80c6639ab388b2326a2ed
Beom Sung Heo <48181483+hbsowo58@users.noreply.github.com>
2025-08-05 16:39:49 +0900

    Update 064p.java

--- 00_web/02_JAVA/064p.java
+++ 00_web/02_JAVA/064p.java
@@ -1,26 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        String[] places = {"제주도", "강릉", "부산", "설악산", "여수"};
-        String[] seasons = {"여름", "겨울", "여름", "가을", "봄"};
+    public static void main(String[] args){
+        String[] places = {"제주도","강릉","부산","설악산","여수"};
+        String[] seasons = {"여름","겨울","여름","가을","봄"};
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("추천받고 싶은 계절을 입력하세요: ");
+        System.out.println("추천받고 싶은 계절을 입력하세요 : ");
         String inputSeason = sc.nextLine();
 
-        boolean found = false;
+        boolean isFound = false;
 
         System.out.println("[추천 여행지]");
-        for (int i = 0; i < places.length; i++) {
-            if (seasons[i].equals(inputSeason)) {
-                System.out.println("- " + places[i]);
-                found = true;
+        for(int i = 0; i < places.length; i++){
+            if(seasons[i].equals(inputSeason)){
+                System.out.println("- "+ places[i]);
+                isFound = true;
             }
         }
-
-        if (!found) {
-            System.out.println("해당 계절의 추천 여행지가 없습니다.");
+        if(!isFound){
+            System.out.println("해당 계절의 추천 여행지가 없습니다");
         }
     }
-} 
+}
