fb06caf4ea6d7446ecd7408c7018993af2b31be5
Beom Sung Heo <48181483+hbsowo58@users.noreply.github.com>
2025-08-19 16:27:16 +0900

    Update 139p_person.js

--- 02_vue/139p_person.js
+++ 02_vue/139p_person.js
@@ -1,8 +1,9 @@
-import { defineStore } from "pinia";
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
 
-export const usePersonStore = defineStore("person", {
-  state: () => ({
-    name: "david",
-    age: 10,
-  }),
-});
+export const usePersonStore = defineStore('person', () => {
+  const name = ref('david')
+  const age = ref(10)
+
+  return { name, age }
+})
