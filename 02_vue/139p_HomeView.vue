6f538d53514dba7a4bb9203f0eea95c09062802e
Beom Sung Heo <48181483+hbsowo58@users.noreply.github.com>
2025-08-19 16:27:30 +0900

    Update 139p_HomeView.vue

--- 02_vue/139p_HomeView.vue
+++ 02_vue/139p_HomeView.vue
@@ -1,29 +1,22 @@
+<script setup>
+import HomeChild from '@/components/HomeChild.vue';
+import { usePersonStore } from '@/stores/person.js';
+const store = usePersonStore()
+// store.count++;
+// store.increment();
+// console.log(store.doubleCount)
+const reducingAge = () => {
+	store.age--;
+}
+</script>
+
 <template>
-  <div style="border: 2px solid black; padding: 10px; width: 350px">
-    <h1>HomeView</h1>
-    <div>{{ name }}</div>
-    <div>{{ age }}</div>
-    <button @click="age--">나이를 줄이자</button>
-    <HomeChild
-      :name="name"
-      :age="age"
-      @changeName="changeName"
-      @setAgeOne="setAgeOne"
-    />
-  </div>
+	<h1>Home View</h1>
+	{{ store.name }}
+	{{ store.age }}
+	<!-- getters : {{ store.doubleCount }} -->
+	<button @click="reducingAge">나이를 줄이자</button>
+	<HomeChild/>
 </template>
 
-<script setup>
-import { ref } from "vue";
-import HomeChild from "./HomeChild.vue";
 
-const name = ref("david");
-const age = ref(10);
-
-function changeName() {
-  name.value = "홍길동";
-}
-function setAgeOne() {
-  age.value = 1;
-}
-</script> 
